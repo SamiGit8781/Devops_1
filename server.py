fa45f2ca06d3f644fc380e4affb69e95d19cedc4
SamiGit8781 <samimb8781@gmail.com>
2025-07-20 00:40:31 +0530

    server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world\n")
+        self.write("Congrats Sami, you have succesfully created container using CI-CD\n")
         print(self.request)
 
 
